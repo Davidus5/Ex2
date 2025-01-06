f1373b8fed7a46e6424c3acef3d686996e189c04
David <kaminerdavid5@gmail.com>
2025-01-06 12:22:55 +0200

    Commit push

---
+++ src/SCellTest.java
@@ -0,0 +1,56 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class SCellTest {
+
+    @org.junit.jupiter.api.Test
+    void isNumber() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isText() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isForm() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void computeForm() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getContent() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getOrder() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testToString() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setData() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testSetData() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getData() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getType() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setType() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setOrder() {
+    }
+}

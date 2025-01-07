65d7cd8881a2ddebe876c560c746d97a166db957
David <kaminerdavid5@gmail.com>
2025-01-07 22:01:45 +0200

    Commit push

--- src/CellEntry.java
+++ src/CellEntry.java
@@ -1,15 +1,51 @@
 // Add your documentation below:
 
 public class CellEntry implements Index2D {
+    private int x; // Column index
+    private int y; // Row index
 
-    @Override
-    public boolean isValid() {
-        return false;
+    // Constructor
+    public CellEntry(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 
+    // Checks if the indices are valid (non-negative)
     @Override
-    public int getX() {return Ex2Utils.ERR;}
+    public boolean isValid() {
+        return x >= 0 && y >= 0;
+    }
 
+    // Get the column index
     @Override
-    public int getY() {return Ex2Utils.ERR;}
+    public int getX() {
+        return x;
+    }
+
+    // Get the row index
+    @Override
+    public int getY() {
+        return y;
+    }
+
+    // Equals method to compare CellEntry objects
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        CellEntry other = (CellEntry) obj;
+        return x == other.x && y == other.y;
+    }
+
+    // Hash code for use in hash-based collections
+    @Override
+    public int hashCode() {
+        return 31 * x + y;
+    }
+
+    // String representation for debugging purposes
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
 }
